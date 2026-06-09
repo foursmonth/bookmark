bc397f08ee9dba1fff6446c81b16f9f48dc260ee
foursmonth <liuyang_18@qq.com>
2026-06-09 23:29:14 +0800

    chore: 删除未使用的 function.ts

--- src/common/function.ts
+++
@@ -1,9 +0,0 @@
-export function isShouldDefaultExpand(nodeDeep: number, expandDeep: number): boolean {
-    return expandDeep < 0 ? true : nodeDeep < expandDeep
-};
-export function deleteElement(list: string[], item: string) {
-    let index = list.indexOf(item)
-    if (index >= 0 && list.length > 0) {
-        list.splice(index,1)
-    }
-}
